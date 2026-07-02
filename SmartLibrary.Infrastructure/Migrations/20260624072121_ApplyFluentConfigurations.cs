0e9719dc32cdc9002d0439c18402590c7d585beb
Mustafa Masoud <moatasemmasoud5@gmail.com>
2026-07-02 06:15:23 +0300

    Adding Services

---
+++ SmartLibrary.Infrastructure/Migrations/20260624072121_ApplyFluentConfigurations.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartLibrary.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyFluentConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
---
+++ SmartLibrary.Infrastructure/Services/AuthorService.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartLibrary.Application.DTOs.Authors.Requests;
+using SmartLibrary.Application.DTOs.Authors.Responses;
+using SmartLibrary.Application.Interfaces;
+using SmartLibrary.Domain.Entities;
+using SmartLibrary.Infrastructure.Data;
+
+namespace SmartLibrary.Infrastructure.Services;
+
+public class AuthorService : IAuthorService
+{
+
+    private readonly AppDbContext _context;
+
+    public AuthorService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<AuthorResponse> CreateAsync(CreateAuthorRequest request , CancellationToken ct)
+    {
+        var author = new Author
+        {
+            Id  = Guid.NewGuid(),
+            Name = request.Name
+        };
+
+        await _context.Authors.AddAsync(author,ct);
+        await _context.SaveChangesAsync();
+
+        return new AuthorResponse
+        {
+            Id = author.Id,
+            Name = author.Name
+        };
+
+    }
+
+    public Task<List<AuthorResponse>> GetAllAsync(CancellationToken ct)
+    {
+        return _context.Authors
+                    .Select (author => new AuthorResponse
+                    {
+                        Id = author.Id,
+                        Name = author.Name
+                    }
+                    ).ToListAsync(ct);
+    }
+
+}
