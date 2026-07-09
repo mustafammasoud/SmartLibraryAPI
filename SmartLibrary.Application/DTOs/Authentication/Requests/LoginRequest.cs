606380c87da799487c7ee822d02e8deed43116bf
Mustafa Masoud <moatasemmasoud5@gmail.com>
2026-07-09 04:05:07 +0300

    Adding Authentication Directory in DTOs

---
+++ SmartLibrary.Application/DTOs/Authentication/Requests/LoginRequest.cs
@@ -0,0 +1,9 @@
+namespace SmartLibrary.Application.DTOs.Authentication.Requests;
+
+
+public class LoginRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    
+}
